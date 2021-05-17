43b019242ec38acc35fed9afcded440c7d0a51c6
Alexandrov911 <sashok17062002@mail.ru>
2021-05-17 12:32:02 +0300

    Практическая 4

---
+++ Практическая 4/2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             //задание 2
+            Console.WriteLine("введите число b от 10");
+            double b = Convert.ToDouble(Console.ReadLine());
+            double a = 1;
+            for (int i=10; i<=b; i++)
+            {
+                a = i * (b*b);
+
+                Console.WriteLine(a);
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Практическая 4/Практическая 4/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практическая_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             // задание 1
+            Console.WriteLine("введите первое число");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("введите второе число");
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("одного из чисел 1 2 3 4 ( которые соответствуют +, -, *, /) ");
+            double z = Convert.ToDouble(Console.ReadLine());
+            double c = 0;
+            switch (z)
+            {
+                case 1:
+                    {
+                        c = x + y;
+                        break;
+                    }
+                case 2:
+                    {
+                        c = x - y;
+                        break;
+                    }
+                case 3:
+                    {
+                        c = x * y;
+                        break;
+                    }
+                case 4:
+                    {
+                        c = x / y;
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Ошибка выбора оператора ");
+                        break;
+                    }
+            }
+            Console.WriteLine("Ответ:" + c);
+            Console.ReadKey();
+
+        }
+    }
+}
